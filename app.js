add743989be21a5635e91ae89f5b6763595ba21f
nutnaik <nutnaik@cisco.com>
2016-11-22 20:17:45 +0530

    Assignment 6.2

---
+++ app.js
@@ -0,0 +1,16 @@
+findGreatestNumber(3,1,5);
+
+function findGreatestNumber(a,b,c){
+	
+	if(a>b && a>c) {
+		console.log("a is greater");
+
+	}
+	else if(b>a && b>c){
+		console.log("b is greater");
+
+	}
+	else {
+		console.log("c is greater");
+	}
+}
